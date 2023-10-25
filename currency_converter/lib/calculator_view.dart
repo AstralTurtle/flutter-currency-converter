f0f181cc1252a86621119d23b9704c7ba5213599
Aaron James <jamesacs329@gmail.com>
2023-10-25 09:04:56 -0400

    added safearea>scaffold>[appbar>text,center>column>textfield]

--- currency_converter/lib/calculator_view.dart
+++ currency_converter/lib/calculator_view.dart
@@ -1,15 +1,33 @@
 import 'package:flutter/material.dart';
 
-class MyWidget extends StatefulWidget {
-  const MyWidget({super.key});
+class CalculatorView extends StatefulWidget {
+  const CalculatorView({super.key});
 
   @override
-  State<MyWidget> createState() => _MyWidgetState();
+  State<CalculatorView> createState() => _CalculatorViewState();
 }
 
-class _MyWidgetState extends State<MyWidget> {
+class _CalculatorViewState extends State<CalculatorView> {
+  final TextEditingController _controller = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SafeArea(
+        child: Scaffold(
+            appBar: AppBar(
+              title: const Text('Currency Converter'),
+            ),
+            body: Center(
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    TextField(
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Enter the amount to convert',
+                      ),
+                      controller: _controller,
+                    ),
+                  ]),
+            )));
   }
 }
