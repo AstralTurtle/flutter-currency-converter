eeda98d390557b4fa5d6286d5b3b3bf273831249
AstralTurtle <jamesacs329@gmail.com>
2023-10-27 09:12:52 -0400

    added getRate function and global apicaller variables.

--- currency_converter/lib/conversion_rate.dart
+++ currency_converter/lib/conversion_rate.dart
@@ -1,11 +1,6 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert' as convert;
 
-void main(List<String> args) {
-  Apicaller a = Apicaller();
-  a.getCurrencies().then((value) => print(value));
-}
-
 class Apicaller {
   http.Client client = http.Client();
   List<String> currencies = [];
@@ -25,5 +20,14 @@ class Apicaller {
     var jsonResponse = convert.jsonDecode(response.body);
     var currencies = jsonResponse.toString().split(',');
     return currencies;
+  }
+
+  Future<double> getRate(String code1, String code2) async {
+    var rateURL = Uri.https('cdn.jsdelivr.net',
+        '/gh/fawazahmed0/currency-api@1/latest/currencies/${code1.toLowerCase()}/${code2.toLowerCase()}.json');
+    var response = await client.get(rateURL);
+    var jsonResponse = convert.jsonDecode(response.body);
+    var rate = jsonResponse['${code2.toLowerCase()}'];
+    return rate;
   }
 }
--- currency_converter/lib/globals.dart
+++ currency_converter/lib/globals.dart
@@ -1,6 +1,8 @@
-library 'globals';
+library globals;
 
 import 'package:flutter/material.dart';
 import 'conversion_rate.dart';
 
 Apicaller api = Apicaller.onAppStart();
+
+List<String> currencies = api.currencies;
